3e7bfe4acf63ec07fa8b76d9325f7a602fff7b00
Aliev Eldar <aliev.eldar.prog@gmail.com>
2025-02-08 13:46:57 +0400

    AddToCart refactoring

--- src/Controller/AddToCartController.php
+++ src/Controller/AddToCartController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Raketa\BackendTestTask\Controller;
 
 use Psr\Http\Message\RequestInterface;
@@ -16,23 +18,65 @@ readonly class AddToCartController
         private ProductRepository $productRepository,
         private CartView $cartView,
         private CartManager $cartManager,
+        private LoggerInterface $logger
     ) {
     }
 
-    public function get(RequestInterface $request): ResponseInterface
+    public function post(RequestInterface $request): ResponseInterface
     {
-        $rawRequest = json_decode($request->getBody()->getContents(), true);
-        $product = $this->productRepository->getByUuid($rawRequest['productUuid']);
-
-        $cart = $this->cartManager->getCart();
-        $cart->addItem(new CartItem(
-            Uuid::uuid4()->toString(),
-            $product->getUuid(),
-            $product->getPrice(),
-            $rawRequest['quantity'],
-        ));
-
         $response = new JsonResponse();
+        $responseCode = 200;
+        try {
+            $rawRequest = json_decode($request->getBody()->getContents(), true);
+
+            if(!isset($rawRequest['productUuid']) || !isset($rawRequest['quantity'])){
+             throw new \HttpInvalidParamException('productUuid not found');
+            }
+
+            $product = $this->productRepository->getByUuid($rawRequest['productUuid']);
+            $cart = $this->cartManager->getCart();
+
+            if(!$cart){
+                $response->getBody()->write(
+                    json_encode(
+                        [
+                            'status' => 'err',
+                            'msg' => 'Cart not found',
+                            'cart' => null,
+                        ],
+                        JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+                    )
+                );
+                $responseCode = 404;
+                return $response
+                    ->withHeader('Content-Type', 'application/json; charset=utf-8')
+                    ->withStatus($responseCode);
+            }
+            $cart->addItem(new CartItem(
+                Uuid::uuid4()->toString(),
+                $product->getUuid(),
+                $product->getPrice(),
+                $rawRequest['quantity'],
+            ));
+
+        }catch (\Exception $exception){
+            $this->logger->error(sprintf('%s :: %s - %s (%s)', __CLASS__, __METHOD__, $exception->getMessage(), $exception->getTraceAsString()));
+            $response->getBody()->write(
+                json_encode(
+                    [
+                        'status' => 'err',
+                        'message' => 'Internal server error',
+                        'cart' => null,
+                    ],
+                    JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+                )
+            );
+            $responseCode = 500;
+            return $response
+                ->withHeader('Content-Type', 'application/json; charset=utf-8')
+                ->withStatus($responseCode);
+        }
+
         $response->getBody()->write(
             json_encode(
                 [
@@ -45,6 +89,6 @@ readonly class AddToCartController
 
         return $response
             ->withHeader('Content-Type', 'application/json; charset=utf-8')
-            ->withStatus(200);
+            ->withStatus($responseCode);
     }
 }
