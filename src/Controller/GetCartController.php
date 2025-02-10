52da0ad695e8ca65da554bd081de94604f43794b
Aliev Eldar <aliev.eldar.prog@gmail.com>
2025-02-10 16:32:27 +0400

    GetCartController refactoring

--- src/Controller/GetCartController.php
+++ src/Controller/GetCartController.php
@@ -13,37 +13,64 @@ readonly class GetCartController
 {
     public function __construct(
         public CartView $cartView,
-        public CartManager $cartManager
+        public CartManager $cartManager,
+        public LoggerInterface $logger
     ) {
     }
 
     public function get(RequestInterface $request): ResponseInterface
     {
         $response = new JsonResponse();
-        $cart = $this->cartManager->getCart();
+        try {
 
-        if (! $cart) {
+            $cart = $this->cartManager->getCart();
+
+            if (!$cart) {
+                $response->getBody()->write(
+                    json_encode(
+                        [
+                            'status' => 'err',
+                            'message' => 'Cart not found',
+                            'cart' => null,
+                        ],
+                        JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+                    )
+                );
+
+                return $response
+                    ->withHeader('Content-Type', 'application/json; charset=utf-8')
+                    ->withStatus(404);
+            } else {
+                $response->getBody()->write(
+                    json_encode(
+                        [
+                            'status' => 'success',
+                            'cart' => $this->cartView->toArray($cart),
+                        ],
+                        JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+                    )
+                );
+            }
+        }catch (\Exception $exception){
+            $this->logger->error(sprintf('%s :: %s - %s (%s)', __CLASS__, __METHOD__, $exception->getMessage(), $exception->getTraceAsString()));
             $response->getBody()->write(
                 json_encode(
-                    ['message' => 'Cart not found'],
+                    [
+                        'status' => 'error',
+                        'message' => 'Internal server error',
+                        'cart' => null,
+                    ],
                     JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
                 )
             );
-
             return $response
                 ->withHeader('Content-Type', 'application/json; charset=utf-8')
-                ->withStatus(404);
-        } else {
-            $response->getBody()->write(
-                json_encode(
-                    $this->cartView->toArray($cart),
-                    JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
-                )
-            );
+                ->withStatus(500);
         }
 
         return $response
             ->withHeader('Content-Type', 'application/json; charset=utf-8')
-            ->withStatus(404);
+            ->withStatus(200);
     }
 }
+
--- src/Controller/GetProductsController.php
+++ src/Controller/GetProductsController.php
@@ -1,35 +1,61 @@
 <?php
 
-declare(strict_types = 1);
+    declare(strict_types=1);
 
-namespace Raketa\BackendTestTask\Controller;
+    namespace Raketa\BackendTestTask\Controller;
 
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
-use Raketa\BackendTestTask\View\ProductsView;
+    use Psr\Http\Message\RequestInterface;
+    use Psr\Http\Message\ResponseInterface;
+    use Raketa\BackendTestTask\View\ProductsView;
 
-readonly class GetProductsController
-{
-    public function __construct(
-        private ProductsView $productsVew
-    ) {
-    }
-
-    public function get(RequestInterface $request): ResponseInterface
+    readonly class GetProductsController
     {
-        $response = new JsonResponse();
+        public function __construct(
+            private ProductsView $productsView,
+            private LoggerInterface $logger
+        ) {
+        }
 
-        $rawRequest = json_decode($request->getBody()->getContents(), true);
+        public function get(RequestInterface $request): ResponseInterface
+        {
+            $response = new JsonResponse();
 
-        $response->getBody()->write(
-            json_encode(
-                $this->productsVew->toArray($rawRequest['category']),
-                JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
-            )
-        );
+            try {
+                $rawRequest = json_decode($request->getBody()->getContents(), true);
+                if (!isset($rawRequest['category'])) {
+                    throw new \InvalidArgumentException("Invalid category");
+                }
+                $response->getBody()->write(
+                    json_encode(
+                        [
+                            'status' => 'success',
+                            'products' => $this->productsView->toArray($rawRequest['category'])
+                        ],
+                        JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+                    )
+                );
+            } catch (\Exception $exception) {
+                $this->logger->error(sprintf('%s :: %s - %s (%s)', __CLASS__, __METHOD__, $exception->getMessage(),
+                    $exception->getTraceAsString()));
 
-        return $response
-            ->withHeader('Content-Type', 'application/json; charset=utf-8')
-            ->withStatus(200);
+                $response->getBody()->write(
+                    json_encode(
+                        [
+                            'status' => 'err',
+                            'message' => 'Internal server error',
+                            'products' => null,
+                        ],
+                        JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+                    )
+                );
+                return $response
+                    ->withHeader('Content-Type', 'application/json; charset=utf-8')
+                    ->withStatus(500);
+            }
+
+
+            return $response
+                ->withHeader('Content-Type', 'application/json; charset=utf-8')
+                ->withStatus(200);
+        }
     }
-}
